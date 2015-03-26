52aeb468cd61a91ecee04b55cc171d6b07b7b035
最萌小汐 <sunyijiusyj@126.com>
2015-03-26 15:38:56 +0800

    独立runtime

--- map/lua/base.lua
+++ map/lua/base.lua
@@ -1,7 +1,6 @@
     jass = require 'jass.common'
 	japi = require 'jass.japi'
 	slk = require 'jass.slk'
-	runtime = require 'jass.runtime'
     hook = require 'jass.hook'
 	storm = require 'jass.storm'
 	suc, console = pcall(require, 'jass.console')
@@ -9,12 +8,6 @@
 		console = nil
 	end
     
-	--打开控制台
-	--runtime.console = true
-	--设置句柄等级为0(地图中所有的句柄均使用table封装)
-	runtime.handle_level = 0
-	--关闭等待功能
-	runtime.sleep = false
 	--拆解table
 	local function sub(t)
 		local meta = getmetatable(t)
