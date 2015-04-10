727d28dd0e0ddf285e441cf4d8666973de1cdbc1
最萌小汐 <sunyijiusyj@126.com>
2015-04-10 10:40:57 +0800

    runtime优先加载

--- map/lua/base.lua
+++ map/lua/base.lua
@@ -1,16 +1,9 @@
     jass = require 'jass.common'
 	japi = require 'jass.japi'
 	slk = require 'jass.slk'
-	runtime = require 'jass.runtime'
     hook = require 'jass.hook'
 	storm = require 'jass.storm'
     
-	--打开控制台
-	--runtime.console = true
-	--设置句柄等级为0(地图中所有的句柄均使用table封装)
-	runtime.handle_level = 0
-	--关闭等待功能
-	runtime.sleep = false
 	--拆解table
 	local function sub(t)
 		local meta = getmetatable(t)
