f5e5c28bdf4eaa41b363ce4c2cfad598cdf9252c
最萌小汐 <sunyijiusyj@126.com>
2015-07-19 15:32:25 +0800

    维护hook

--- map/lua/hook.lua
+++ map/lua/hook.lua
@@ -1,18 +1,23 @@
 
-	function hook.UnitWakeUp(u, f)
-        game.initHero(u)
-    end
-    
-    function hook.SetUnitRescueRange(u, r, f)
-        if jass.GetUnitTypeId(u) == 0 then
-            if r + 0 == 23333 then
-		        cmd.start()
-	        end
-            return
+function hook_UnitWakeUp(u, f)
+	game.initHero(u)
+end
+
+function hook_SetUnitRescueRange(u, r, f)
+    if jass.GetUnitTypeId(u) == 0 then
+        if r + 0 == 23333 then
+	        cmd.start()
         end
-        if r < 0 then
-            game[math.floor(0 - r)](u)
-        else
-            return f(u, r)
-        end
+        return
     end
+    if r < 0 then
+        game[math.floor(0 - r)](u)
+    else
+        return f(u, r)
+    end
+end
+
+timer.loop(30, true, function()
+	hook.UnitWakeUp = hook_UnitWakeUp
+	hook.SetUnitRescueRange = hook_SetUnitRescueRange
+end)
