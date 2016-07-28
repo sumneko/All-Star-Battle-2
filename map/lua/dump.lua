879271162fce1b739a8daf5aba13fce1774aed88
最萌小汐 <sunyijiusyj@126.com>
2016-07-28 15:15:32 +0800

    dump文件版本更新到5.3

--- map/lua/dump.lua
+++ map/lua/dump.lua
@@ -2,19 +2,17 @@
 	dump	= {}
 	local dump	= dump
 	setmetatable(dump, dump)
-
-	--此luac文件的版本为5.2
+	
+	--此luac文件的版本为5.3
 	local result	= pcall(require, 'lua\\dumpc')
 	
-	--目前11平台的lua引擎版本为5.2,如果acb成功回归11平台,将lua引擎版本更新为5.3或更高版本后,该luac文件将会加载失败
-	--希望他能加载失败吧~
-	----写于2014年12月27日
 	if result and dump.save then
 		dump.enable	= true
 		print 'dump enbale'
 	else
 		dump.enable	= false
 		print 'dump disable'
+		print('result', result, 'dump.save', dump.save)
 		
 		function dump.save()
 			return ''
